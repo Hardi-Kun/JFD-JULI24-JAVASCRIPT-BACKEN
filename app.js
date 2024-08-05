550ff0240ff38b732f13ce03f27c6d1e2207c8e0
Hardi-Kun <m.hardiansyah0406@gmail.com>
2024-08-05 20:50:04 +0700

    first commit, yes i do

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+
+http.createServer( function(request, respon){
+    // mendeteksi status 200 (user berhasil terkoneksi dengan aplkasi kita)
+    // Content-type: apa tipe konten yg akan diberikan ke user 
+    // tex/plain itu akan menampilkan teks apa adanya
+    // text/html akan merender tag html menjadi tampilan di browser
+    respon.writeHead(200, {'Content-type': 'text/plain'})
+    // hasil akhir yg akan diberikan ke user
+    respon.end('Halo Guys')
+}).listen(3000, function() {
+    console.log('Server sudah siap, buka http://localhost:3000/')
+})
+    
