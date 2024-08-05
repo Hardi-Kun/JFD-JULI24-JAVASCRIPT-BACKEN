9bc9f75fa5cb57f03b94ae8fd9ac73f6b1f23dbc
Hardi-Kun <m.hardiansyah0406@gmail.com>
2024-08-05 21:02:32 +0700

    ganti respon end

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ http.createServer( function(request, respon){
     // Content-type: apa tipe konten yg akan diberikan ke user 
     // tex/plain itu akan menampilkan teks apa adanya
     // text/html akan merender tag html menjadi tampilan di browser
-    respon.writeHead(200, {'Content-type': 'text/plain'})
+    respon.writeHead(200, {'Content-type': 'text/html'})
     // hasil akhir yg akan diberikan ke user
-    respon.end('Halo Guys')
+    respon.end('<h1>Halo Guys</h1>')
 }).listen(3000, function() {
     console.log('Server sudah siap, buka http://localhost:3000/')
 })
