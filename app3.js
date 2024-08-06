26f4cbdc3e87689858efbd0ff5fb42eb29a49ce3
Hardi-Kun <m.hardiansyah0406@gmail.com>
2024-08-06 19:59:50 +0700

    Percobaan html terpisah

--- app3.js
+++ app3.js
@@ -1,20 +1,23 @@
 const http = require('http')
+const fs   = require('fs')
 
 let server = http.createServer( function(request, respon){
+    console.log(request)
     if (request.url == '/') {
         respon.writeHead(200, {'Content-type': 'text/html'})
         respon.write('<h1>Halaman Beranda</h1>')
         respon.end()
     } else if (request.url == '/profil') {
-        respon.writeHead(200, {'Content-type': 'text/html'})
-        respon.write('<h1>Ini adalah profil saya</h1>')
-        respon.end()
+        fs.createReadStream('./view/profil.html').pipe(respon)
+    } else if (request.url == '/berita') {
+        fs.createReadStream('./view/berita.html').pipe(respon)
     } else {
         respon.writeHead(200, {'Content-type': 'text/html'})
         respon.write('<h1>404 Not Found</h1>')
         respon.end()
     }
 })
+
 
 
 server.listen(3000, function() {
