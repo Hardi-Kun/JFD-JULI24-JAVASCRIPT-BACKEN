db02eda93b42d075bdea94637be59e513355a471
Hardi-Kun <m.hardiansyah0406@gmail.com>
2024-08-09 21:59:57 +0700

    ejs kirim data dan logic

--- app-express.js
+++ app-express.js
@@ -1,6 +1,10 @@
-const express =  require('express')
+const express =  require ('express')
 const app = express()
 const port = 3000
+
+
+app.set('view engine', 'ejs') // setting penggunaan
+app.set('views', './view-ejs') // setting penggunaan 
 
 
 app.get('/', function (req, res) {
@@ -9,9 +13,27 @@ app.get('/', function (req, res) {
 
 //route --> rute
 app.get('/hubungi', function(req, res) {
-    res.send('<h1>Silahkan WA ke: 0833939392</h1>')
+    let data_hub = {
+        No_Telp: '085959576221',
+        Email: 'mhardiansyah0406@gmail.com',
+        Ig: 'mhardisyh',
+    
+    }
+    res.render('hubungi', data_hub)
 })
 
+
+app.get('/profil', function(req, res) {
+    let data = {
+        jabatan : 'Senior Programmer',
+        gender : 'Laki',
+        gaji: 10000000
+
+    }
+    res.render('profil-developer',data)
+    // error, karena express tidak membaca file dengan extensi .html
+    // res.send(require('./view/profil.html'))
+})
 
 app.listen(port, function() {
     console.log(`Server sudah siap, buka http://localhost:` +port)
