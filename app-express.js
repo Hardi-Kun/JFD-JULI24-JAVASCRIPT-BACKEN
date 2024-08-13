6b4a2100426efd625c162b1d87943201dde66f05
Hardi-Kun <m.hardiansyah0406@gmail.com>
2024-08-13 20:51:34 +0700

    membuat form tambahan

--- app-express.js
+++ app-express.js
@@ -143,6 +143,10 @@ function hapus_satuKaryawan(idk) {
 // proses penghapusan data END
 
 
+app.get('/karyawan/tambah', function (req, res){
+    res.render('karyawan/form-tambah')
+})
+
 app.listen(port, function() {
     console.log(`Server sudah siap, buka http://localhost:` +port)
 })
