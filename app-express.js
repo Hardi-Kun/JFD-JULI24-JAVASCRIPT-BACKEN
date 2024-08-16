03b8dce3b59049c8ad476eac8f20641b6b5a008c
Hardi-Kun <m.hardiansyah0406@gmail.com>
2024-08-16 21:58:31 +0700

    buat controller

--- app-express.js
+++ app-express.js
@@ -10,10 +10,13 @@ app.set('view engine', 'ejs') // setting penggunaan template engine untuk expres
 app.set('views', './view-ejs') // setting penggunaan folder untuk menyimpan file .ejs
 
 
-// include
+// include masing-masing model
 const m_karyawan    = require('./models/m_karyawan')
 const m_departemen  = require('./models/m_departemen')
 const m_agama       = require('./models/m_agama')
+
+//  include masing-masing controller
+const c_karyawan    = require('./controller/c_karyawan')
 
 
 app.get('/', function (req, res) {
@@ -44,94 +47,17 @@ app.get('/profil', function(req, res) {
     // res.send(require('./view/profil.html'))
 })
 
+app.get('/karyawan', c_karyawan.index)
+app.get('/karyawan/detail/:id_karyawan', c_karyawan.detail)
+app.get('/karyawan/hapus/:id_karyawan', c_karyawan.hapus)
+app.get('/karyawan/tambah', c_karyawan.tambah)
+app.get('/karyawan/proses-insert', c_karyawan.insert)
+app.get('/karyawan/edit/:id_karyawan', c_karyawan.edit)
+app.get('/karyawan/proses-update/:id_karyawan', c_karyawan.update)
 
 // gunakan async await, untuk memaksa node js
 // menunggu script yg dipanggil sampai selesai dieksekusi
 // async dan await selalu berpasangan
-app.get('/karyawan', async function(req, res) {
-    // ambil object
-    let dataview = {
-        karyawan: await m_karyawan.get_semuakaryawan(),
-        message: req.query.msg,
-    }
-    res.render('karyawan/index', dataview)
-})
-// proses pengambilan data dari mysql END
-
-
-// proses pengambilan id dari db OPEND
-app.get('/karyawan/detail/:id_karyawan', async function(req, res){
-    //  ambil id yg dikirim via url
-    let idk = req.params.id_karyawan
-
-    // setelah itu kirim ke proses request data mysql
-    let dataview = {
-        pegawai: await m_karyawan.get_satukaryawan(idk)
-    }
-    res.render('karyawan/detail', dataview)
-})
-
-
-app.get('/karyawan/hapus/:id_karyawan', async function (req, res){
-    //  ambil id yg dikirim via url
-    let idk = req.params.id_karyawan
-    
-    // proses hapus data
-    try {
-        let hapus = await m_karyawan.hapus_satuKaryawan(idk)
-        if (hapus.affectedRows > 0) {
-                res.redirect('/karyawan')
-        }
-    } catch (error) {
-        throw error
-    }  
-})
-
-
-app.get('/karyawan/tambah', async function (req, res){
-    let dataview = {
-        dept: await m_departemen.get_semuaDepartemen(),
-        agm: await m_agama.get_semuaAgama(),
-    }
-    res.render('karyawan/form-tambah', dataview)
-})
-
-
-app.post('/karyawan/proses-insert', async function (req,res){
-    // terima kiriman data dari form html
-    // let body = req.body
-    try {
-        let insert = await m_karyawan.insert_karyawan(req)
-        if (insert.affectedRows > 0) {
-            res.redirect(`/karyawan?msg=berhasil tambah karyawan a/n ${req.body.form_nama_lengkap}`)
-        }
-    } catch (error) {
-        throw error
-    }
-})
-
-app.get('/karyawan/edit/:id_karyawan', async function(req, res) {
-    let idk = req.params.id_karyawan
-    let dataview = {
-        dept    : await m_departemen.get_semuaDepartemen(),
-        agm     : await m_agama.get_semuaAgama(),
-        pegawai : await m_karyawan.get_satukaryawan(idk),
-    }
-    res.render('karyawan/form-edit', dataview)
-})
-
-
-app.post('/karyawan/proses-update/:id_karyawan', async function(req,res) {
-    let idk = req.params.id_karyawan
-    try {
-        let update = await m_karyawan.update_karyawan(req, idk)
-        if (update.affectedRows > 0) {
-            res.redirect(`/karyawan?msg=berhasil edit karyawan a/n ${req.body.form_nama_lengkap}`)
-        }
-    } catch (error) {
-        throw error
-    }
-})
 
 
 app.listen(port, function() {
